8f8c841a37734350ef44cce5f63f41f09fa16ce7
jeromedecoster <jeromedecoster@users.noreply.github.com>
2017-10-10 13:32:11 +0200

    0.0.3

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 
 if [[ `git rev-parse --is-inside-work-tree 2>/dev/null` == 'true' ]]; then
     echo offline install
+elif [[ `pwd` == '/tmp/dots' ]]; then
+    echo /tmp/dots install
 else
     echo online install
+    mkdir -p /tmp/dots
+    cd /tmp/dots
+    curl -sSL https://github.com/jeromedecoster/test2/archive/master.tar.gz | tar zx --strip 1
+    #bash ./install.sh
 fi
